f6907ed7f7c98124cc3aa59d689e5ad9e8027f52
lakhan jindam <lakhanj569@gmail.com>
2024-12-27 20:25:05 +0530

    feat: add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import Contact from './components/Contact';
 import Certifications from './components/certifications/certifications';
 import Skills from './components/Skills';
 import ScrollToTop from './components/ScrollToTop';
+import { Analytics } from "@vercel/analytics/react"
 
 const App: React.FC = () => {
   return (
     <div className="bg-gray-900">
+      <Analytics />
       <Hero />
       <Timeline />
       <Skills />
